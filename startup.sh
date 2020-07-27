90569eb755c90952c09b95385c3ccf6ba2854a3e
Ethnical <Ethnicalnightmare@gmail.com>
2020-07-27 17:12:49 +0200

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+echo "What you want do to ? "
+echo "================================================================================================================="
+echo "1. Connect via SSH [DEFAULT]"
+echo "2. Nothing"
+echo -e "================================================================================================================= \n"
+read -p "Choice : " Choice
+hex=$(echo "$Choice" |xxd -p) 
+if [ "$hex" == '0a' ] || [ $Choice == "1" ]; then #default
+    ssh root@172.16.0.100 
+fi
+if [ $Choice == "Nothing" ] || [ $Choice == "2" ]; then
+    exit
+fi
