d3b5f5590916d6b3f8daafe32a9196bce21984d6
Ethnical <Ethnicalnightmare@gmail.com>
2020-07-27 02:40:10 +0200

    Create tmux_choose.sh

---
+++ tmux_choose.sh
@@ -0,0 +1,50 @@
+#!/bin/bash 
+ARRAY=()
+RES=0
+p=""
+
+if [[ -z "${TMUX}" ]]; then  #Detect if $TMUX is set.
+	while [[ -z "$name" ]]
+	do
+        echo "SESSIONS AVALAIBLE :"
+		echo "================================================================================================================="
+		tmux ls
+		echo "new: Create a new session."
+		echo "No: Not using TMUX"
+		echo -e "================================================================================================================= \n"
+		arr=$(tmux ls |awk '{print $1}' | cut -d ":" -f1)
+		while read p; do
+  			ARRAY+=($p)	
+		done <<< "$arr"
+		echo "Which session do we need ?"
+		read -p "Session Name : " name
+		echo ""
+		
+		if [[ $name == "new" ]]; then #Create Session
+			read -p "Name for the session? : " session
+			tmux new -s $session
+			exit
+		fi
+		if [[ $name == "No" ]]; then
+			exit
+		fi
+		if [[ " ${ARRAY[*]} " == *$name* ]]; then
+			tmux attach -t $name
+		else
+			if [[ $name == *"video"* ]]; then #VideoSession
+			    tmux new -s $name -dP
+				tmux set-environment -t $name HOME /home/rootube
+				tmux send-keys -t $name:0 "export HOME=/home/rootube;reset;neofetch" C-m #R
+				tmux attach -t $name
+				exit
+			fi
+			tput clear
+			tput setaf 3 
+			echo "Retry with the following sessions :"
+			tput clear
+			name=""
+		
+		fi
+	done	
+	
+fi
