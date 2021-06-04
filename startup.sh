c65fccae6a2bc9cc1153bd0563d194cc1af47c99
Ethnical <Ethnicalnightmare@gmail.com>
2021-06-04 22:50:33 +0200

    Add vscode check

--- startup.sh
+++ startup.sh
@@ -1,14 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 echo "What you want do to ? "
 echo "================================================================================================================="
 echo "1. Connect via SSH [DEFAULT]"
 echo "2. Nothing"
 echo -e "================================================================================================================= \n"
+if [[ $WSLENV == *"VSCODE_WSL_EXT_LOCATION"* ]]; then #default
+	   reset
+	   exit
+fi	
 read -p "Choice : " Choice
-hex=$(echo "$Choice" |xxd -p) 
+hex=$(echo "$Choice" |xxd -p)
+	
 if [ "$hex" == '0a' ] || [ "$Choice" == "1" ]; then #default
-    ssh root@172.16.0.100 
+	    ssh root@172.16.0.100
 fi
-if [ "$Choice" == "Nothing" ] || [ "$Choice" == "2" ]; then
-    exit
+if [ "$Choice" == "Nothing" ] || [ "$Choice" == "2" ]; then 
+     	exit
 fi
